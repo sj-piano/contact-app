5917baeaade4acd9d7c7bafeb6a32efe68e6874a
StJohn Piano <stjohnpiano@gmail.com>
2023-10-30 19:04:45 +0000

    Phone number now only accepts digits

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -1,10 +1,16 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class ContactSchema(BaseModel):
     name: str = Field(..., min_length=3, max_length=256)
     phone: str = Field(..., min_length=9, max_length=50)
     email: str = Field(..., min_length=6, max_length=256)
+
+    @validator("phone")
+    def validate_phone_digits(cls, phone):
+        if not phone.isdigit():
+            raise ValueError("Phone number must only contain digits")
+        return phone
 
 
 class ContactDB(ContactSchema):
